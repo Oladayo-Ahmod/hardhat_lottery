dcde746a3322857a81bc112570892c5bbe7fb7ee
oladayo ahmod <oladayoahmod112@gmail.com>
2022-08-07 15:40:40 +0100

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-waffle")
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+        {
+            version: "0.8.9",
+        },
+        {
+            version: "0.4.24",
+        },
+    ],
+},
+};
