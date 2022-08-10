030a7c67b2716c647d532286826ee25a2dc179c0
oladayo ahmod <oladayoahmod112@gmail.com>
2022-08-10 14:58:04 +0100

    vfr coordinator mocks deployment

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,3 +1,4 @@
+const { ethers } = require('hardhat');
 const {developmentChains,networkConfig } = require('../hardhat.config')
 
 module.exports  = async function({deployments,getNamedAccounts}){
@@ -5,8 +6,19 @@ module.exports  = async function({deployments,getNamedAccounts}){
     const {deployer} = await getNamedAccounts();
     const chainId = network.config.chainId
 
+    const BASE_FEE = ethers.utils.parseEther("0.25") //base fee
+    const GAS_PRICE_LINK = 1e9 //link per gas
+    const args = [BASE_FEE,GAS_PRICE_LINK]
     if (developmentChains.includes(network.name)) {
         log('local network detected')
+        await deploy("VrfCoordinator",{
+            from : deployer,
+            args : args,
+            log : true
+        })
+        log('Mocks deployed')
+        log('-----------------------------------')
     }
 
 }
+module.exports.tags = ["all","mocks"]
