824aae443310aedf8026c4c3f5358b3a954facba
oladayo ahmod <oladayoahmod112@gmail.com>
2022-08-16 16:08:04 +0100

    dynamic front-end constants update

--- deploy/99-update-front-end.js
+++ deploy/99-update-front-end.js
@@ -1,15 +1,35 @@
-const { ethers } = require("ethers")
+const { ethers } = require("hardhat")
 const fs = require('fs')
-const FRONT_END_ABI_FILE = '../../nextjs-hardhat-lottery/constants/contractAddress.json'
-const FRONT_END_ADDRESS_FILE = '../../nextjs-hardhat-lottery/constants/abi.json'
+const FRONT_END_ABI_FILE = '../nextjs-hardhat-lottery/constants/contractAddress.json'
+const FRONT_END_ADDRESS_FILE = '../nextjs-hardhat-lottery/constants/abi.json'
 
 module.exports = function(){
-    if(process.env.UPDATE-FRON_END){
+    if(process.env.UPDATE_FRON_END){
         console.log('updating front-end')
-        updateFrontend()
+        updateContractAddress()
+        updateAbi()
     }
 }
-async function updateFrontend(){
+async function updateContractAddress(){
     const contract = await ethers.getContract('Raffle')
-    const currentAddress = json.parse(fs.readFileSync(FRONT_END_ADDRESS_FILE))
+    const chainId = network.config.chainId.toString()
+    const currentAddress = JSON.parse(fs.readFileSync(FRONT_END_ADDRESS_FILE,'utf8'))    
+    // const currentAbi = JSON.parse(fs.readFileSync(FRONT_END_ABI_FILE,'utf8'))
+
+    if (chainId in currentAddress) { // check for chain id
+        if (!currentAddress[chainId].includes(contract.address)) { // check if contract address exist
+            currentAddress[chainId].push(contract.address)
+        }
+    }
+    else{
+        currentAddress[chainId] = [contract.address] // update contract address if 
+    }
+    fs.writeFileSync(FRONT_END_ADDRESS_FILE,JSON.stringify(currentAddress)) // write updated address back to the file
 }
+
+async function updateAbi(){
+    const contract = await ethers.getContract('Raffle')
+    fs.writeFileSync(FRONT_END_ABI_FILE,contract.interface.format(ethers.utils.FormatTypes.json))
+}
+
+module.exports.tags = ["all","frontend"]
